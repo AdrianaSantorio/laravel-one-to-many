518107aa4d3407de9be2b5693e3a363ab93e1c93
AdrianaSantorio <adriana.santorio@gmail.com>
2022-04-10 09:59:12 +0200

    populated category table

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -11,6 +12,18 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = [
+            ['label' => 'HTML', 'color' => 'info'],
+            ['label' => 'CSS', 'color' => 'danger'],
+            ['label' => 'PHP', 'color' => 'warning'],
+            ['label' => 'Laravel', 'color' => 'success'],
+            ['label' => 'VueJS', 'color' => 'primary']
+        ];
+
+        foreach ($categories as $category) {
+            $newCategory = new Category();
+            $newCategory->fill($category);
+            $newCategory->save();
+        }
     }
 }
